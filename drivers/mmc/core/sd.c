6905be681fc364a1aff134303df4e13bf9dbe453
Denis Benato <benato.denis96@gmail.com>
2025-05-13 16:20:37 +0000

    Revert "mmc: core: sd: set a maximum speed to avoid stability issues"
    This reverts commit f5aefec8acd68fd922861ed2a46523507e7d0d46.

--- drivers/mmc/core/sd.c
+++ drivers/mmc/core/sd.c
@@ -30,8 +30,6 @@
 #include "sd.h"
 #include "sd_ops.h"
 
-#define MAX_SD_CARD_SPEED 12500000U
-
 static const unsigned int tran_exp[] = {
 	10000,		100000,		1000000,	10000000,
 	0,		0,		0,		0
@@ -519,9 +517,7 @@ static int sd_set_bus_speed_mode(struct mmc_card *card, u8 *status)
 			mmc_hostname(card->host));
 	else {
 		mmc_set_timing(card->host, timing);
-		const unsigned selected_clock = card->sw_caps.uhs_max_dtr > MAX_SD_CARD_SPEED ? MAX_SD_CARD_SPEED : card->sw_caps.uhs_max_dtr;
-		pr_warn("UHS-I SD card new clock set to %u Hz", selected_clock);
-		mmc_set_clock(card->host, selected_clock);
+		mmc_set_clock(card->host, card->sw_caps.uhs_max_dtr);
 	}
 
 	return 0;
@@ -1525,10 +1521,7 @@ retry:
 		/*
 		 * Set bus speed.
 		 */
-		const unsigned supported_clock = mmc_sd_get_max_clock(card);
-		const unsigned selected_clock = supported_clock > MAX_SD_CARD_SPEED ? MAX_SD_CARD_SPEED : supported_clock;
-		pr_warn("SD card new clock set to %u Hz", selected_clock);
-		mmc_set_clock(host, selected_clock);
+		mmc_set_clock(host, mmc_sd_get_max_clock(card));
 
 		if (host->ios.timing == MMC_TIMING_SD_HS &&
 			host->ops->prepare_sd_hs_tuning) {
